ff4fa0edade273ebc590858895ed961d529bc1cb
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2023-06-07 20:19:22 +0800

    This program checks if the number is a prime number or not.

---
+++ PrimeNumberChecker.java
@@ -0,0 +1,22 @@
+public class PrimeNumberChecker {
+    public static void main(String args[])
+    {		
+     int num = 29;
+     boolean isPrime=true;
+
+     for(int i=2; i<=num/2; ++i)
+     {
+
+        if(num % i == 0)
+        {
+           isPrime = false;
+           break;
+        }
+     }
+     //If isPrime is true then the number is prime else not
+     if(isPrime)
+        System.out.println(num + " is a Prime Number");
+     else
+        System.out.println(num + " is not a Prime Number");
+    }
+}
