31821ad78aada908d0483f9497435c3651a82fab
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2023-06-07 18:52:50 +0800

    Create a loop that compares the max value to the next number in the array. If the value is larger, then it will be saved as the new max.

--- MaximumValue.java
+++ MaximumValue.java
@@ -3,7 +3,15 @@ public class MaximumValue {
   
         int [] numbers = {1, 3, 5, 7, 9, 2, 4, 6, 8};  
         int max = numbers[0];  
-        
+
+        //Loop through the array
+        for (int i = 0; i < numbers.length; i++) {  
+            //compare elements with the max value
+            if(numbers[i] > max)
+            // if the number is larger, then it will be the new max
+                max = numbers[i]; 
+         }
+
         System.out.println("The maximum value in the array is: " + max);  
     }  
 }  
